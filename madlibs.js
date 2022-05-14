e966348676dcc0d33be0f421f056fcfd16e82835
NouhRastanawi <noahrastdark@gmail.com>
2022-05-14 12:20:23 +0300

    Fetching and creating elements

--- madlibs.js
+++ madlibs.js
@@ -1,42 +1,58 @@
-/**
- * Complete the implementation of parseStory.
- * 
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- * 
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- * 
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- * 
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- * 
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
-function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+
+function fetchStory(rawStory) {
+    return fetch('./story.txt')
+    .then(response => response.text()).then(processedStory => {
+      console.log(processedStory);
+      parseStory(processedStory);
+
+    });
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- * 
- * You'll want to use the results of parseStory() to display the story on the page.
- */
+// fetchStory();
+
+const FormMadlibs = document.getElementById("FormMadlibs");
+
+function parseStory(processedStory) {
+  const arrayStory = processedStory.split(" ");
+  console.log(arrayStory);
+  const regexN = /[[n]]/;
+  const regexV = /[[v]]/;
+  const regexA = /[[a]]/;
+  for(let i of arrayStory) {
+    if(regexN.test(i)) {
+      console.log("Noun:", i);
+      const elementNoun = document.createElement("input");
+      elementNoun.innerText = i;
+      // elementNoun.textContent = i;
+      FormMadlibs.appendChild(elementNoun);
+      // function to create elements
+
+    }
+    else if(regexV.test(i)) {
+      const elementNoun = document.createElement("input");
+      elementNoun.innerText = i;
+      
+      // function to create elements
+    }
+    else if(regexA.test(i)) {
+      const elementNoun = document.createElement("input");
+      elementNoun.innerText = i;
+      
+      // function to create elements
+    }
+    else {
+      const elementNoun = document.createElement("label");
+      // elementNoun.innerText = i + " ";
+      elementNoun.textContent = i + " ";
+      FormMadlibs.appendChild(elementNoun);
+    }
+
+  }
+}
+
+
+
 getRawStory().then(parseStory).then((processedStory) => {
   console.log(processedStory);
 });
+
